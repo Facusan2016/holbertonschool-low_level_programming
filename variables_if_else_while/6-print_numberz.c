1ac98b3de30ab38a31e05901730a5dd60c237b9f
Facusan2016 <facusan2016@gmail.com>
2023-06-15 06:40:13 -0700

    Exercise 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int crt;
+
+	for (crt = 48; crt <= 57; crt++)
+	{
+		putchar(crt);
+	}
+	putchar('\n');
+	return (0);
+}
