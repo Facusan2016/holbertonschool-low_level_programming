267c8eb378ce5ca1bee2dc6885e2687b846e029e
Facusan2016 <facusan2016@gmail.com>
2023-06-14 17:40:16 -0700

    Exercise 0

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -3,7 +3,7 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)
- */
+ */with proper grammar, but the outcome is a piece of art,
 int main(void)
 {
 	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
