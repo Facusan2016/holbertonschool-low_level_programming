65371facfbefda8d2891422521a9a5a50cadeb98
Facusan2016 <facusan2016@gmail.com>
2023-06-16 16:46:11 -0700

    Exercise 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - Prints the result of adding two integers.
+ * @num1: First integer.
+ * @num2: Second integer.
+ * Return: The result of the addition.
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
