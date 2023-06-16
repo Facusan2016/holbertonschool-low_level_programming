19554340639b1529c2292ea5298221f4e85d2fd7
Facusan2016 <facusan2016@gmail.com>
2023-06-15 17:25:49 -0700

    Exercise 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet()
+{
+	char iter;	
+	for(iter = 'a'; iter <= 'z'; iter++)
+	{
+		_putchar(iter);
+	}
+	_putchar('\n');
+}
