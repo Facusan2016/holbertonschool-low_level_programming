e820a681ddc7c9ab25dc253d9cb0678af92e9e69
Facusan2016 <facusan2016@gmail.com>
2023-06-15 18:49:58 -0700

    Exercise 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - Write a function that makes a number absolute.
+ * @n: Is an int value.
+ *
+ * Return: The absolute value of n.
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
