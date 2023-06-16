45c5bf081922c233a267159fedff373c235e67f6
Facusan2016 <facusan2016@gmail.com>
2023-06-16 10:40:34 -0700

    Exercise 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -19,26 +19,26 @@ void times_table(void)
 
 				if (j > 0)
 				{
-					putchar (',');
-					putchar (' ');
-					putchar (' ');
+					_putchar (',');
+					_putchar (' ');
+					_putchar (' ');
 				}
 
 			}
 			else
 			{
 
-				putchar (',');
-				putchar (' ');
-				putchar ((j * i) / 10 + '0');
+				_putchar (',');
+				_putchar (' ');
+				_putchar ((j * i) / 10 + '0');
 
 			}
 
-			putchar ((j * i) % 10 + '0');
+			_putchar ((j * i) % 10 + '0');
 
 			if (j == 9)
 			{
-				putchar('\n');
+				_putchar('\n');
 			}
 
 
