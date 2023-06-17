8fd3d9620fb29dde37437c2445b095d3f98ac70b
Facusan2016 <facusan2016@gmail.com>
2023-06-16 22:12:24 -0700

    Exercise 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - Short description, single line
+ * Return: Nothing.
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for(i = '0'; i <= '9'; i++)
+		_putchar(i);
+
+	_putchar('\n');
+
+}
+
+
