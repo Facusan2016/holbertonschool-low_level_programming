11cd981ed0908617cbe32fcdacdeaa95403f7e9f
Facusan2016 <facusan2016@gmail.com>
2023-06-21 12:46:46 -0700

    Exercise 7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * puts_half - Write the half of a string.
+ * @str: a pointer to a char variable
+ * Return: Nothing.
+ */
+
+void puts_half(char *str)
+{
+	int count = 0;
+
+	while (str[count] != '\0')
+		count++;
+
+	if (count % 2 == 0)
+		count = (count / 2);
+	else
+		count = (count - 1) / 2;
+
+	while (str[count] != '\0')
+	{
+		_putchar(str[count]);
+		count++;
+	}
+
+	_putchar('\n');
+}
+
