0259a7d4e09d87329023f3b4eb7d0f6a6cff582f
Facusan2016 <facusan2016@gmail.com>
2023-06-21 09:41:46 -0700

    Exercise 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_rev - Write a functions that prints a string in reverse.
+ * @s: a pointer to a char variable
+ * Return: Nothing.
+ */
+
+void print_rev(char *s)
+{
+	int iter = 0;
+
+	while (s[iter] != '\0')
+	{
+		iter++;
+	}
+
+	while(iter >= 0)
+	{
+		_putchar(s[iter]);
+		iter--;
+	}
+
+	_putchar('\n');
+}
+
