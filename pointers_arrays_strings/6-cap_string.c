f6dc5c4ea291284fcdeb076ee373819a55a329d1
Facusan2016 <facusan2016@gmail.com>
2023-06-22 10:48:41 -0700

    Exercise 6, project 2

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * cap_string - Change all lowercase letters to uppercase.
+ * @c: a pointer to a char.
+ * Return: a char.
+ */
+
+char *cap_string(char *c)
+{
+	char q[14] = {'\n','\t',' ',',',';','.','!','?','"','(',')','{','}','\0'};
+
+	int i, cnt1, cnt2;
+
+	for(cnt1 = 0; c[cnt1] != '\0'; cnt1++)
+	{
+		if(c[cnt1] < 'a' || c[cnt1] > 'z')
+		{
+			cnt2 = 0;
+
+			while(c[cnt1] != q[cnt2] && q[cnt2] != '\0')
+				cnt2++;
+
+			if(q[cnt2] != '\0' && c[cnt1 - 2] != '\0')
+				c[cnt1 + 1] = c[cnt1 + 1] - 32;
+		}
+	}
+
+	return (c);
+}
