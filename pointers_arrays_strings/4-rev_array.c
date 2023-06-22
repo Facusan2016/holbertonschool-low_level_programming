79ad39eced3f0235e77df555e0623bee792d95e9
Facusan2016 <facusan2016@gmail.com>
2023-06-21 18:04:43 -0700

    Exercise 4, Project 2

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * reverse_array - Compares two Strings.
+ * @a: a pointer to a String.
+ * @n: a pointer to another String.
+ * Return: Nothing.
+ */
+
+void reverse_array(int *a, int n)
+{
+
+	int i, j;
+	int aux;
+
+	i = 0;
+	j = n - 1;
+
+	while ((i + j) == n - 1 && i < j)
+	{
+		aux = a[i];
+		a[i] = a[j];
+		a[j] = aux;
+
+		i++;
+	}
+
+}
+
