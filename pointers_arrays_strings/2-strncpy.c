edc3079a699d5344f6c4f4cba08a596d37107385
Facusan2016 <facusan2016@gmail.com>
2023-06-23 05:46:15 -0700

    Exercise 2, Project 3

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -11,7 +11,7 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int iter = 0;
 
-	while (iter < n && src[iter] != '\0')
+	while (src[iter] != '\0' && iter < n)
 	{
 		dest[iter] = src[iter];
 		iter++;
