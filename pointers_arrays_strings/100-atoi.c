6fe810898135e016af47aecce9fca6bd75dcb8b7
Facusan2016 <facusan2016@gmail.com>
2023-06-23 15:08:58 -0700

    Exercise 10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -22,7 +22,14 @@ int _atoi(char *s)
 
 		while (s[iter] >= '0' && s[iter] <= '9' && flag == 0)
 		{
-			atoiNum = (atoiNum * 10 + (s[iter] - 48));
+			if(atoiNum < 0)
+			{
+				atoiNum = (atoiNum * 10 + (s[iter] - 48));
+			}else
+			{
+				atoiNum = -1 * (atoiNum * 10 + (s[iter] - 48) * -1);
+			}
+
 			if (!(s[iter + 1] >= '0' && s[iter + 1] <= '9'))
 				flag = 1;
 			iter++;
