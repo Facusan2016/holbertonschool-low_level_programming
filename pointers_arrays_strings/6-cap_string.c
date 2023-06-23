2af1f40f636f33f9cb9e6515aa5cb961323bce6f
Facusan2016 <facusan2016@gmail.com>
2023-06-22 17:52:20 -0700

    Exercise 6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -10,7 +10,7 @@ char *cap_string(char *c)
 	char q[] = ",;.!?(){}\n\t\" ";
 	int cnt1, cnt2;
 
-	if(c[0] != '\0' && c[0] >= 'a' && c[0] <= 'z')
+	if (c[0] != '\0' && c[0] >= 'a' && c[0] <= 'z')
 		 c[0] = c[0] - 32;
 
 	for (cnt1 = 0; c[cnt1] != '\0'; cnt1++)
