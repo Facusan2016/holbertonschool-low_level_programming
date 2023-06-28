158611a3c7675089efc23a8bc8c8f6a0f920481c
Facusan2016 <facusan2016@gmail.com>
2023-06-28 06:01:22 -0700

    Exercise 1

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - Write a functions that prints a string in reverse.
+ * @s: a pointer to a String
+ * Return: Nothing.
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(++s);
+		_putchar(*s);
+	}
+}
+
