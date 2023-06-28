8d663e6b6fc0079d591a8c7c68435ac8bcd1e983
Facusan2016 <facusan2016@gmail.com>
2023-06-28 11:53:58 -0700

    Exercise 5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -11,8 +11,8 @@ int sqr_re(int n, int it)
 {
 	if (n == it * it)
 		return (it);
-	else if (n != it * it && it > 2)
-		return (sqr_re(n, it - 1));
+	else if (n != it * it && it <= n / 2)
+		return (sqr_re(n, it + 1));
 	else
 		return (-1);
 }
@@ -27,8 +27,6 @@ int _sqrt_recursion(int n)
 {
 	if (n <= 0)
 		return (-1);
-	else if (n == 1)
-		return (1);
 	else
-		return (sqr_re(n, n / 2));
+		return (sqr_re(n, 1));
 }
