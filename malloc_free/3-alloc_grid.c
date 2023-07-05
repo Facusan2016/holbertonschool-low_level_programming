73501b9b27631d88c3ebc99a060b004e548861e8
Facusan2016 <facusan2016@gmail.com>
2023-07-04 17:58:41 -0700

    Exercise 3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -29,7 +29,12 @@ int **alloc_grid(int width, int height)
 		p[i] = malloc(sizeof(int) * width);
 
 		if (p[i] == NULL)
+		{
+			for (; i >= 0; i--)
+				free(p[i]);
+
 			return (NULL);
+		}
 	}
 
 	return (p);
