60c2fb3220fbc719359cbff75d9593a637b7e17f
Facusan2016 <facusan2016@gmail.com>
2023-07-04 17:36:58 -0700

    Exercise 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 /**
  * str_concat - Concatenates two Strings.
@@ -13,17 +14,16 @@ char *str_concat(char *s1, char *s2)
 {
 	int i, j;
 
-	int size1 = strlen(s1);
-
-	int size2 = strlen(s2);
+	int size1 = 0;
+	int size2 = 0;
 
 	char *p;
 
-	if (s1 == NULL)
-		size1 = 0;
+	if (s1 != NULL)
+		size1 = strlen(s1);
 
-	if (s2 == NULL)
-		size2 = 0;
+	if (s2 != NULL)
+		size2 = strlen(s2);
 
 	p = malloc(sizeof(char) * size1 + sizeof(char) * size2 + 1);
 
@@ -35,7 +35,7 @@ char *str_concat(char *s1, char *s2)
 
 	j = 0;
 
-	for (; i < (size1 + size2); i++)
+	for (; i < (size1 + size2) && s2 != NULL; i++)
 	{
 		p[i] = s2[j];
 		j++;
