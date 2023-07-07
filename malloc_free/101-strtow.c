442010b63780d69e744293002ac1a04f6834e5e1
Facusan2016 <facusan2016@gmail.com>
2023-07-07 09:34:31 -0700

    Exercise 6

---
+++ malloc_free/101-strtow.c
@@ -0,0 +1,72 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * wordcnt - Count how many words separated by a space are in a string.
+ * @str: Word to be counted.
+ * Return: Quantity.
+ */
+
+int wordcnt(char *str)
+{
+	int iter = 0, wc = 0;
+
+	while (str[iter] != '\0')
+	{
+		if (str[iter] != ' ' && str[iter - 1] == ' ')
+			wc++;
+		iter++;
+	}
+	return (wc);
+}
+
+/**
+ * strtow - Splits a string into words.
+ * @str: String to be splited.
+ * Return: String.
+ */
+
+char **strtow(char *str)
+{
+	int i = 0, count = 0, j = 0;
+	char **p;
+
+	p = malloc(sizeof(*p) * (wordcnt(str)));
+
+	if (p == NULL)
+		return (NULL);
+
+	for (; i < (wordcnt(str)); i++)
+	{
+		while (str[j] == ' ')
+			j++;
+		while (str[j] != ' ' && str[j] != '\0')
+		{
+			count++;
+			j++;
+		}
+		p[i] = malloc(sizeof(char) * (count + 1));
+		count = 0;
+
+		if (p[i] == NULL)
+			return (NULL);
+	}
+	j = 0;
+	for (i = 0; i < (wordcnt(str)); i++)
+	{
+		while (str[j] == ' ')
+			j++;
+		count = 0;
+		while (str[j] != ' ' && str[j] != '\0')
+		{
+			p[i][count] = str[j];
+			count++;
+			j++;
+		}
+		p[i][count] = '\0';
+	}
+	p[i] = NULL;
+	return (p);
+}
