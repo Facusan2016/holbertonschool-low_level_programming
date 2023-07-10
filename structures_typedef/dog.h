ade2b7ce807a1ec901309f096efefddfc8f5fd7d
Facusan2016 <facusan2016@gmail.com>
2023-07-10 09:45:30 -0700

    Exercise 3, fixed dog.h betty styles

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -10,25 +10,15 @@
  * Description: This type defines the main characteristics of a dog.
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-/**
- * dog_t - New name for the type struct dog
- * @name: Name of the dog.
- * @age: Age of the dog.
- * @owner: Owner of the dog.
- *
- * Description : A new name for the dog struct
- */
-
-typedef struct dog dog_t;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
 
 #endif
