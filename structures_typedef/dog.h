fbac93095f72df7a3f7531fa27a6ab28b081c79e
Facusan2016 <facusan2016@gmail.com>
2023-07-10 04:58:53 -0700

    Exercise 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+ * struct dog - Struct for a dog.
+ * @name: Name of the dog.
+ * @age: Age of the dog.
+ * @owner: Owner of the dog.
+ *
+ * Description: This type defines the main characteristics of a dog.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
