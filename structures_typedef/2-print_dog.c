8ab0066509b1c220c7fd4bb2c79fc720a92f57fe
Facusan2016 <facusan2016@gmail.com>
2023-07-10 07:16:44 -0700

    Exercise 2

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,35 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_dog - Write a function that prints a dog's characteristics.
+ * @d: Struct to be initialized
+ * Return: Nothing.
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+			printf("Name: (nil)\n");
+		else if (d->name != NULL)
+			printf("Name: %s\n", d->name);
+
+		if (d->age == 0)
+			printf("Age: (nil)\n");
+		else if (d->age != 0)
+			printf("Age: .6%f\n", d->age);
+
+		if (d->owner == NULL)
+			printf("Owner: (nil)\n");
+		else if (d->owner != NULL)
+			printf("Owner: %s\n", d->owner);
+
+	} else
+	{
+		printf("%s\n", "");
+	}
+}
+
