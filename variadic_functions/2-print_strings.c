923e916eedb372587f83c4981053519dad53d414
Facusan2016 <facusan2016@gmail.com>
2023-07-12 05:59:07 -0700

    Exercise 2, case NULL in the end fixed

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -36,6 +36,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	}
 
 	str = va_arg(arg, char *);
+
+	if (str == NULL)
+		str = "(nil)";
+
 	printf("%s\n", str);
 
 	va_end(arg);
