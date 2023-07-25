b90a44c726c5e76b77b595c307181939b2b88ed4
Facusan2016 <facusan2016@gmail.com>
2023-07-25 15:49:58 -0700

    fix problem with function selected

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -29,7 +29,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 
 	if (q->next == NULL && idx == 1)
-		return (add_dnodeint(h, n));
+		return (add_dnodeint_end(h, n));
 
 	p = malloc(sizeof(dlistint_t));
 
