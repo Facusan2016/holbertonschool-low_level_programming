b6b79d1b2a6618fe092f5f65f5ccd6f20caefc11
Facusan2016 <facusan2016@gmail.com>
2023-08-02 19:45:40 -0700

    fix betty styles

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -9,23 +9,26 @@
 
 void hash_table_print(const hash_table_t *ht)
 {
-	long unsigned int it_arr = 0;
+	unsigned long int it_arr = 0;
 	char n = 0;
 	hash_node_t *iter;
 
-	printf("{");
-	while (ht != NULL && it_arr < ht->size)
+	if (ht != NULL)
 	{
-		iter = ht->array[it_arr];
-		while (iter != NULL)
+		printf("{");
+		while (it_arr < ht->size)
 		{
-			if (n)
-				printf(", ");
-			printf("'%s': '%s'", iter->key, iter->value);
-			iter = iter->next;
-			n = 1;
+			iter = ht->array[it_arr];
+			while (iter != NULL)
+			{
+				if (n)
+					printf(", ");
+				printf("'%s': '%s'", iter->key, iter->value);
+				iter = iter->next;
+				n = 1;
+			}
+			it_arr++;
 		}
-		it_arr++;
+		printf("}\n");
 	}
-	printf("}\n");
 }
