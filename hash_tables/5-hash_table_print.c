89a9d0b57a4e49a614fb0bc2e4276bb446ac2334
Facusan2016 <facusan2016@gmail.com>
2023-08-02 19:42:25 -0700

    feat printing hash tables

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * hash_table_print - Function that prints a hash table.
+ * @ht: Hash table.
+ * Return: Nothing.
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	long unsigned int it_arr = 0;
+	char n = 0;
+	hash_node_t *iter;
+
+	printf("{");
+	while (ht != NULL && it_arr < ht->size)
+	{
+		iter = ht->array[it_arr];
+		while (iter != NULL)
+		{
+			if (n)
+				printf(", ");
+			printf("'%s': '%s'", iter->key, iter->value);
+			iter = iter->next;
+			n = 1;
+		}
+		it_arr++;
+	}
+	printf("}\n");
+}
